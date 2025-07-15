ac11ddd1982e2961bb7ea19a92401a790246892c
Tiffany Messer <tiffanymesser15@gmail.com>
2025-07-15 16:14:46 -0400

    Updating the getAll to match frontend

--- src/artworks/schemas/response.schema.ts
+++ src/artworks/schemas/response.schema.ts
@@ -1,7 +1,9 @@
 import { HttpStatus } from '@nestjs/common';
+import { Pagination } from '../artworks/artworks.service';
 
 export interface ResponseObject<T> {
   status: HttpStatus;
+  pagination: Pagination;
   message: string;
   data?: T;
   error?: string;
